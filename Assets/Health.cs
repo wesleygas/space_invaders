6b45fa8e7fbb64961ee737504a467bc9215b58cf
Fogelman <ddfogelman@gmail.com>
2020-02-22 15:12:10 -0300

    ADD Health status with spaceships

---
+++ Assets/Health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+    public int health = 3;
+    public GameObject spaceships;
+
+    void Start () { }
+    // Update is called once per frame
+    void Update () {
+
+        int children = spaceships.transform.childCount;
+        for (int i = 0; i < children; i++) {
+            if (i < health) {
+                spaceships.transform.GetChild (i).gameObject.SetActive (true);
+            } else {
+                spaceships.transform.GetChild (i).gameObject.SetActive (false);
+            }
+        }
+
+    }
+}
